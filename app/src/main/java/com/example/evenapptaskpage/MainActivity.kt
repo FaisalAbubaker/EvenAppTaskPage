21839758e5c95c8815d08e955751b5c346098996
FaisalAbubaker <faiabu95@gmail.com>
2024-05-14 21:20:40 +0300

    m

--- app/src/main/java/com/example/evenapptaskpage/MainActivity.kt
+++ app/src/main/java/com/example/evenapptaskpage/MainActivity.kt
@@ -1,41 +1,27 @@
 package com.example.evenapptaskpage
 
 import android.icu.text.SimpleDateFormat
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.DateRange
-import androidx.compose.material.icons.filled.KeyboardArrowLeft
-import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -45,33 +31,22 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.AbsoluteAlignment
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.evenapptaskpage.ui.theme.EvenAppTaskPageTheme
-import java.time.DayOfWeek
 import java.time.LocalDate
-import java.time.format.DateTimeFormatter
-import java.time.format.FormatStyle
-import java.time.temporal.ChronoUnit
 import java.util.Date
-import java.util.stream.Collectors
-import java.util.stream.Stream
 import com.foreverrafs.datepicker.DatePickerTimeline
-import com.foreverrafs.datepicker.Orientation
-import com.foreverrafs.datepicker.state.rememberDatePickerState
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.isActive
 
 
 class MainActivity : ComponentActivity() {
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -136,7 +111,11 @@ fun TaskPage() {
         DatePickerTimeline(
             modifier = Modifier,
             onDateSelected = {selectedDate: LocalDate->
-                date = selectedDate.toString()
+                if (selectedDate == LocalDate.now()){
+                    date = "Today"
+                    } else {
+                    date = selectedDate.toString()
+                }
             }
         )
         Row(
@@ -165,7 +144,7 @@ fun TaskPage() {
                             .height(114.dp),
                         verticalAlignment = Alignment.CenterVertically
                     ) {
-                        Text(text = it)
+                        Text(text = it,)
                         Text(
                             text = "      You don't have any schedule   or  ",
                             color = Color.Gray
